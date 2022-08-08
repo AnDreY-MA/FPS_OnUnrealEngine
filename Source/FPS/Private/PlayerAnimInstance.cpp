f6337c8b44a29ca3362031d7aca227a4b1c37598
AnDreY-MA <andreymamontov9@gmail.com>
2022-08-08 13:15:21 +0300

    Добавлена анимация ходьбы
    Реализован класс PlayerAnimInstance отвечающий за анимацию игрового персонажа

---
+++ Source/FPS/Private/PlayerAnimInstance.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerAnimInstance.h"
+
+#include "CharacterController.h"
+
+void UPlayerAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	ACharacterController* Player = Cast<ACharacterController>(TryGetPawnOwner());
+	if(Player != nullptr)
+	{
+		FVector Velocity = Player->GetVelocity();
+		Velocity.Z = 0;
+		Speed = Velocity.Size();
+	}
+}
